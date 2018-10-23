1a6e69d8a0bbcd09f0f20128faf2ea5ccc350ece
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-10-23 14:34:37 +0200

    Created the start to Sector Five

---
+++ games/sector_five/sector_five.rb
@@ -0,0 +1,13 @@
+require 'gosu'
+
+class SectorFive < Gosu::Window
+    
+    def initialize
+        super(800, 600)
+        self.caption = 'Sector Five'
+    end
+
+end
+
+window = SectorFive.new
+window.show
