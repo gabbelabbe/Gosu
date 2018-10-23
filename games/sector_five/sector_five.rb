3ba4c7c29b517f649faeecd3fcaead0047d37b6b
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-10-23 14:50:38 +0200

    added so it draws the ship(player)

--- games/sector_five/sector_five.rb
+++ games/sector_five/sector_five.rb
@@ -6,8 +6,12 @@ class SectorFive < Gosu::Window
     def initialize
         super(800, 600)
         self.caption = 'Sector Five'
+        @player = Player.new(self)
     end
 
+    def draw
+        @player.draw
+    end
 end
 
 window = SectorFive.new
