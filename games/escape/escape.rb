f52379d251e52bd7d46eff81e5c7094fdfe45918
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-10-26 11:13:27 +0200

    added some stuff for gravity and so on

--- games/escape/escape.rb
+++ games/escape/escape.rb
@@ -2,10 +2,17 @@ require 'gosu'
 require 'chipmunk'
 
 class Escape < Gosu::Window
-
+	DAMPING = 0.9
+	GRAVITY = 400.0
+	
 	def initialize
 		super 800,800,false
 		self.caption = "Escape"
+		@game_over = false
+		@space = CP::Space.new
+		@background = Gosu::Image.new('images/background.png', tileable: true)
+		@space.damping = DAMPING
+		@space.gravity = CP::Vec2.new(0.0, GRAVITY)
 	end
 
 	def draw
@@ -13,7 +20,11 @@ class Escape < Gosu::Window
 	end
 
 	def update
-	
+		unless @game_over
+			10.times do
+				@space.step(1.0 / 600)
+			end
+		end
 	end
 end
 
